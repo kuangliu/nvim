5a8469833c0be5186f544c38dac9cbf49da7c49b
忆扬 <liukuang.lk@alibaba-inc.com>
2021-10-26 17:17:07 +0800

    Add surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -68,6 +68,11 @@ require('packer').startup(function()
     'rcarriga/nvim-dap-ui', 
     requires = {'mfussenegger/nvim-dap'}
   }
+
+  use {
+    'blackCauldron7/surround.nvim',
+    config = function()
+      require('surround').setup {mappings_style = 'surround'}
+    end
+  }
 end)
-
-
