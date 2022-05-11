e9fce7e2fec4f96439001068327684b2446b507b
忆扬 <liukuang.lk@alibaba-inc.com>
2022-05-11 17:35:09 +0800

    Update luasnip cmp key

--- init.lua
+++ init.lua
@@ -80,7 +80,13 @@ require('bufferline').setup({
 ----------------------------------
 -- Nvim-cmp
 ----------------------------------
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -94,7 +100,28 @@ cmp.setup({
     { name = 'path' },
   },
   mapping = {
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   experimental = {
